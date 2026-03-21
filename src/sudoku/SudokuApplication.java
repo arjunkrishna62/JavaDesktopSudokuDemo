e4688a9ae52ba11f53ab7eac25e2fe8b16e9aefb
Arjun.Krishna <arjunkrishna1282@gmail.com>
2026-03-21 20:18:24 +0530

    game basic file structure

---
+++ src/sudoku/SudokuApplication.java
@@ -0,0 +1,26 @@
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class SudokuApplication extends Application {
+    private IUserInterfaceContract.View uiImpl;
+
+    @Override
+    public static start(String primaryStage) throws Exception {
+       uiImpl = new UserInterfaceImpl(primaryStage);
+
+       try {
+        SudokuBuildLogic.build(uiImpl);
+       } catch (IOException e){
+        e.printStackTrace();
+        throw e;
+       }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/sudoku/constants/GameState.java
@@ -0,0 +1,5 @@
+public enum GameState {
+    COMPLETE,
+    ACTIVE,
+    NEW
+}
